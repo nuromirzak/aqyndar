497dc71680a2fc9bb72b9bc026866e75d056793f
Nurmukhammed <76807326+nuromirzak@users.noreply.github.com>
2022-07-12 06:40:19 +0600

    fix: rename import

--- config.js
+++ config.js
@@ -6,8 +6,6 @@ const mongoURI = process.env.MONGO_DB;
 
 const secretString = "apple_orange_lemon";
 
-const {storage} = require("./cloudinary_config");
-
 const defaultProfilePicture = {
     url: "https://res.cloudinary.com/de7j5tens/image/upload/v1657535099/zerdeleu-images/893_ybnxhy.png",
     filename: "zerdeleu-images/893_ybnxhy.png",
@@ -16,6 +14,5 @@ const defaultProfilePicture = {
 module.exports = {
     mongoURI,
     secretString,
-    storage,
     defaultProfilePicture,
 }
--- routers/signUpRouter.js
+++ routers/signUpRouter.js
@@ -3,11 +3,11 @@ const router = express.Router();
 
 const signUpController = require('../controllers/signUpController');
 const multer = require("multer");
-const {storageConfig} = require("../config");
+const storage = require("../cloudinary_config").storage;
 
 router.get('/', signUpController.displaySignUp);
 
-const upload = multer({storage: storageConfig});
+const upload = multer({storage: storage});
 router.post('/', upload.single("profilepic"), signUpController.signUp);
 
 module.exports = router;
