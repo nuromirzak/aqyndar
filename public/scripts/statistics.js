ad1686697935d4a835dcfdace3e1862a67ff4bd7
Nurmukhammed <76807326+nuromirzak@users.noreply.github.com>
2022-07-31 20:46:44 +0600

    feat: add statistics div

--- public/scripts/statistics.js
+++ public/scripts/statistics.js
@@ -2,18 +2,18 @@ const authorsNum = document.querySelector('#statistics #authorsNum');
 const poemsNum = document.querySelector('#statistics #poemsNum');
 const linesNum = document.querySelector('#statistics #linesNum');
 const annotationsNum = document.querySelector('#statistics #annotationsNum');
-const usersNum = document.querySelector('#statistics #usersNum');
+// const usersNum = document.querySelector('#statistics #usersNum');
 
 const refreshStatistics = async () => {
     const response = await fetch('/api/statistics');
 
     const data = await response.json();
 
-    authorsNum.innerHTML = `${data.authorsNum} автор қосылды`;
-    poemsNum.innerHTML = `${data.poemsNum} өлең қосылды`;
-    linesNum.innerHTML = `${data.linesNum} тармақ қосылды`;
-    annotationsNum.innerHTML = `${data.annotationsNum} аннотация қосылды`;
-    usersNum.innerHTML = `${data.usersNum} пайдаланушы тіркелді`;
+    authorsNum.innerHTML = `${data.authorsNum}`;
+    poemsNum.innerHTML = `${data.poemsNum}`;
+    linesNum.innerHTML = `${data.linesNum}`;
+    annotationsNum.innerHTML = `${data.annotationsNum}`;
+    // usersNum.innerHTML = `${data.usersNum} пайдаланушы тіркелді`;
 };
 
 refreshStatistics().then(r => console.log('Statistics refreshed')).catch(e => console.log(e));
